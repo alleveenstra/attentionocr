098d781697384032402eb1e851fea60f01f1bf12
Alle Veenstra <alle.veenstra@gmail.com>
2019-11-02 10:24:26 +0100

    Automatically detect packages in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-from distutils.core import setup
+import setuptools
 
-setup(name='attentionocr',
+setuptools.setup(name='attentionocr',
       version='0.1',
       description='Attention OCR in pure Keras',
       author='Alle Veenstra',
       author_email='alle.veenstra@gmail.com',
       url='https://www.github.org/alleveenstra/attentionocr/',
-      packages=['attentionocr']
+      packages=setuptools.find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
 )
