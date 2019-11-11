ee8f18c5605b8d24feda4ac511ad8da50216d427
Alle Veenstra <alle.veenstra@gmail.com>
2019-11-11 17:36:41 +0100

    Removed duplicate code

--- tests/datasource.py
+++ tests/datasource.py
@@ -8,15 +8,13 @@ class DataSourceTest(unittest.TestCase):
 
     def test_csv(self):
         source = CSVDataSource('.', 'sample.txt')
-        counter = 0
-        for filename, label in source:
-            counter += 1
-            self.assertTrue(os.path.isfile(filename))
-            self.assertIn(label, ['test_50x16', 'test_100x32', 'test_288x32', 'test_600x100'])
-        self.assertEqual(4, counter)
+        self.check_source(source)
 
     def test_flat(self):
         source = FlatDirectoryDataSource('test_*.png')
+        self.check_source(source)
+
+    def check_source(self, source):
         counter = 0
         for filename, label in source:
             counter += 1
