6ecb07f452bd361b80d0ee4ddbb72e97beb4e7e3
Alle Veenstra <alle.veenstra@gmail.com>
2019-11-16 22:09:54 +0100

    Push loss to tensorboard

---
+++ attentionocr/layers.py
@@ -0,0 +1,60 @@
+from typing import Tuple
+
+import tensorflow as tf
+from tensorflow.keras import Input, Sequential
+from tensorflow.keras.layers import MaxPool2D, Conv2D, LSTM, BatchNormalization, Dense
+
+
+class Encoder:
+    layers = [Conv2D(64, (3, 3), padding='same', activation='relu'),
+              MaxPool2D(strides=(2, 2), padding='valid'),
+              BatchNormalization(),
+              Conv2D(128, (3, 3), padding='same', activation='relu'),
+              MaxPool2D(strides=(2, 2), padding='valid'),
+              BatchNormalization(),
+              Conv2D(256, (3, 3), padding='same', activation='relu'),
+              MaxPool2D(strides=(2, 1), padding='valid'),
+              BatchNormalization(),
+              Conv2D(256, (3, 3), padding='valid', activation='relu'),
+              MaxPool2D(strides=(2, 1), padding='valid'),
+              BatchNormalization()]
+
+    def __init__(self, units):
+        self.cnn = Sequential(self.layers)
+        self.lstm = LSTM(units, return_sequences=True, return_state=True)
+
+    def __call__(self, encoder_input) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+        out = self.cnn(encoder_input)
+        out = tf.squeeze(out, axis=1)
+        return self.lstm(out)
+
+
+class Attention:
+    def __init__(self, units: int):
+        # https://papers.nips.cc/paper/7181-attention-is-all-you-need.pdf
+        self.query_projection = Dense(units)
+
+    def __call__(self, decoder_input, encoder_output) -> Tuple[tf.Tensor, tf.Tensor]:
+        query = self.query_projection(decoder_input)
+        key = encoder_output
+        value = encoder_output
+        logits = tf.matmul(query, tf.transpose(key, perm=[0, 2, 1]))
+        attention_weights = tf.nn.softmax(logits)
+        context_vectors = tf.matmul(attention_weights, value)
+        return [context_vectors, attention_weights]
+
+
+class Decoder:
+    def __init__(self, units):
+        self.lstm = LSTM(units, return_sequences=True, return_state=True)
+
+    def __call__(self, decoder_input, initial_state) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+        return self.lstm(decoder_input, initial_state)
+
+
+class DecoderOutput:
+    def __init__(self, vocab_size):
+        self.dense = Dense(vocab_size, activation='softmax')
+
+    def __call__(self, decoder_output) -> tf.Tensor:
+        return self.dense(decoder_output)
--- attentionocr/model.py
+++ attentionocr/model.py
@@ -1,16 +1,15 @@
 import datetime
 import os
-from typing import List, Tuple
+from typing import List
 
 import cv2
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras.backend as K
 from tensorflow.keras import Input, Sequential
-from tensorflow.keras.layers import MaxPool2D, Conv2D, LSTM, BatchNormalization, Dense
 from tqdm import tqdm
 
-from attentionocr import metrics, Vocabulary
+from attentionocr import metrics, Vocabulary, Encoder, Attention, Decoder, DecoderOutput
 
 
 class AttentionOCR:
@@ -67,6 +66,7 @@ class AttentionOCR:
     def fit_generator(self, generator, steps_per_epoch: int = 1, epochs: int = 1, validation_data=None, validate_every_steps: int = 10) -> None:
         optimizer = tf.optimizers.RMSprop()
         K.set_learning_phase(1)
+        accuracy = 0
         for epoch in range(epochs):
             tensorboard_loss = tf.keras.metrics.Mean()
             tensorboard_accuracy = tf.keras.metrics.Mean()
@@ -88,7 +88,8 @@ class AttentionOCR:
                     y_pred = self._inference_model(x)
                     accuracy = metrics.masked_accuracy(y_true, y_pred)
                     tensorboard_accuracy(accuracy)
-                    pbar.set_postfix({"accuracy": "%.4f" % accuracy, "loss": "%.4f" % loss.numpy()})
+                tensorboard_loss(loss.numpy())
+                pbar.set_postfix({"accuracy": "%.4f" % accuracy, "loss": "%.4f" % loss.numpy()})
                 with self.summary_writer.as_default():
                     tf.summary.scalar('epoch_loss_avg', tensorboard_loss.result(), step=optimizer.iterations)
                     tf.summary.scalar('epoch_accuracy', tensorboard_accuracy.result(), step=optimizer.iterations)
@@ -135,60 +136,3 @@ class AttentionOCR:
                 cv2.imwrite('out/%s-%d-%s.png' % (text, index, text[index]), filtered_image)
 
         return texts
-
-
-class Encoder:
-    layers = [Conv2D(64, (3, 3), padding='same', activation='relu'),
-              MaxPool2D(strides=(2, 2), padding='valid'),
-              BatchNormalization(),
-              Conv2D(128, (3, 3), padding='same', activation='relu'),
-              MaxPool2D(strides=(2, 2), padding='valid'),
-              BatchNormalization(),
-              Conv2D(256, (3, 3), padding='same', activation='relu'),
-              MaxPool2D(strides=(2, 1), padding='valid'),
-              BatchNormalization(),
-              Conv2D(256, (3, 3), padding='valid', activation='relu'),
-              MaxPool2D(strides=(2, 1), padding='valid'),
-              BatchNormalization()]
-
-    def __init__(self, units):
-
-        self.cnn = Sequential(self.layers)
-
-        self.lstm = LSTM(units, return_sequences=True, return_state=True)
-
-    def __call__(self, encoder_input) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
-        out = self.cnn(encoder_input)
-        out = tf.squeeze(out, axis=1)
-        return self.lstm(out)
-
-
-class Attention:
-    def __init__(self, units: int):
-        # https://papers.nips.cc/paper/7181-attention-is-all-you-need.pdf
-        self.query_projection = Dense(units)
-
-    def __call__(self, decoder_input, encoder_output) -> Tuple[tf.Tensor, tf.Tensor]:
-        query = self.query_projection(decoder_input)
-        key = encoder_output
-        value = encoder_output
-        logits = tf.matmul(query, tf.transpose(key, perm=[0, 2, 1]))
-        attention_weights = tf.nn.softmax(logits)
-        context_vectors = tf.matmul(attention_weights, value)
-        return [context_vectors, attention_weights]
-
-
-class Decoder:
-    def __init__(self, units):
-        self.lstm = LSTM(units, return_sequences=True, return_state=True)
-
-    def __call__(self, decoder_input, initial_state) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
-        return self.lstm(decoder_input, initial_state)
-
-
-class DecoderOutput:
-    def __init__(self, vocab_size):
-        self.dense = Dense(vocab_size, activation='softmax')
-
-    def __call__(self, decoder_output) -> tf.Tensor:
-        return self.dense(decoder_output)
