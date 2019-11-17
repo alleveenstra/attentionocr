4e7c2b46a09209a3722bcaf7e2ea4a98294286aa
Alle Veenstra <alle.veenstra@gmail.com>
2019-11-17 11:33:50 +0100

    Increased CNN size

--- attentionocr/focus.py
+++ attentionocr/focus.py
@@ -12,10 +12,10 @@ from .model import Encoder
 class Focus:
     def create_focus(self, filename):
         if not os.path.exists(filename):
-            return -np.ones((42, 76))
+            return -np.ones((42, 77))
         with open(filename) as f:
             meta = json.load(f)
-        q = np.zeros((42, 76))
+        q = np.zeros((42, 77))
         for index, char in enumerate(meta):
             x0 = char['x']
             x1 = x0 + char['width']
@@ -23,11 +23,11 @@ class Focus:
                 if type(layer) is MaxPool2D:
                     x0 /= float(layer.pool_size[1])
                     x1 /= float(layer.pool_size[1])
-                if type(layer) is Conv2D:
-                    x0 -= math.ceil(layer.kernel_size[1] / 2.)
-                    x1 += math.ceil(layer.kernel_size[1] / 2.)
+                # if type(layer) is Conv2D:
+                #     x0 -= math.ceil(layer.kernel_size[1] / 2.)
+                #     x1 += math.ceil(layer.kernel_size[1] / 2.)
             x0 = max(math.floor(x0), 0)
-            x1 = min(math.ceil(x1), 76)
+            x1 = min(math.ceil(x1), 77)
             q[index, x0:x1] = 10.0
         q = tf.nn.softmax(q, axis=0)
         return q
